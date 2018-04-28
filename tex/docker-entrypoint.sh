2d46e0cb958a53047ba5f18c4199b4fe429d0d2b
shuymn <mail@shuymn.me>
2018-04-29 04:13:22 +0900

    feat(tex): add tex directory

---
+++ tex/docker-entrypoint.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -eu
+
+# diaplay error
+function error() {
+  echo "$@" 1>&2
+  exit 1
+}
+
+# check number of argument
+[[ "$#" -eq 0 ]] && error "Error: invalid argument."
+# check filetype
+[[ "$1" -ne *.tex ]] && error "Error: invalid filetype."
+# remove a extension
+filename="${1%.*}"
+# generate pdf
+cd /mnt
+uplatex "${filename}.tex" && dvipdfmx "${filename}.dvi"
+# after-treatment
+case "$?" in
+  0 ) find /mnt -type f | grep -E "${filename}\.(aux|dvi|log|out)$" | xargs rm && exit 0 ;;
+  1 ) find /mnt -type f | grep -E "${filename}\.(aux|dvi|log|out)$" | xargs rm && exit 1 ;;
+  * ) exit 2;;
+esac
